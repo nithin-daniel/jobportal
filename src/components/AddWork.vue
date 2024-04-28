a18e51cdef97d9a165d180a9233ba2f6fef95580
Nithin Daniel <nithindaniel2018@gmail.com>
2024-04-28 23:40:03 +0530

    job posting done

--- src/components/AddWork.vue
+++ src/components/AddWork.vue
@@ -1,23 +1,28 @@
 <script setup>
 import Navbar from './Navbar.vue';
 import Footer from './Footer.vue';
+import { collection, addDoc,getFirestore } from "firebase/firestore";
+// import db from '../../firebase/firebase.js'
+const db = getFirestore();
 
 import { ref } from 'vue'
 const overview = ref('')
 const youget = ref('')
 const skills = ref('')
+const job_type = ref('')
 
-const addWork = async() => {
-    const docRef = await addDoc(collection(dbnew, "works"), {
+const addWork = async () => {
+    const docRef = await addDoc(collection(db, "works"), { 
         user: localStorage.user_id,
-        email: localStorage.email,
-        overview:overview ,
-        youget:youget,
-        skills:skills
+        email: localStorage.user_email,
+        overview: overview.value,
+        youget: youget.value,
+        skills: skills.value,
+        job_type:job_type.value
     });
     console.log("Document written with ID: ", docRef.id);
+   
 }
-addWork()
 </script>
 <template>
     <div class="">
@@ -28,7 +33,7 @@ addWork()
         <hr>
         <div class="col-12">
             <div class="single-input">
-                <input type="text" placeholder="Overview" name="Overview" id="Overview" required v-model="Overview">
+                <input type="text" placeholder="Overview" name="Overview" id="Overview" required v-model="overview">
             </div>
             <div class="single-input">
                 <input type="text" placeholder="What You Will Get" name="youget" id="youget" required v-model="youget">
@@ -36,7 +41,14 @@ addWork()
             <div class="single-input">
                 <input type="text" placeholder="Skills" name="skills" id="skills" required v-model="skills">
             </div>
-            <div class="col-12 mb-25"><button class="ht-btn" @click="register">Add Work</button></div>
+            <div class="single-input pb-2">
+                <label for="">Job Type</label>
+                <select name="cars" id="cars" v-model="job_type">
+                    <option value="parttimr" selected>Part Time</option>
+                    <option value="household">House Hold</option>
+                </select>
+            </div>
+            <div class="col-12 mb-25"><button class="ht-btn" @click="addWork">Add Work</button></div>
         </div>
     </div>
     <div class="">
