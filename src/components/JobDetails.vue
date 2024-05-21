c5f62236e9a06cf3c041e35a6cac4037b80bd97a
Nithin Daniel <nithindaniel2018@gmail.com>
2024-05-21 07:04:05 +0530

    added 3 second delay in job details

--- src/components/JobDetails.vue
+++ src/components/JobDetails.vue
@@ -10,6 +10,7 @@ import { loadStripe } from '@stripe/stripe-js'
 // import { createCheckoutSession } from '@/api/stripe'; // Replace with your API call to create a Checkout session
 import { ref, defineComponent, onBeforeMount } from 'vue';
 import { v4 as uuidv4 } from 'uuid';
+import { useRouter } from 'vue-router' // import router
 
 
 export default {
@@ -72,21 +73,22 @@ export default {
                 },
             });
 
-            if(this.job.job_type == "household") {
+            if (this.job.job_type == "household") {
 
                 [this.job.email, localStorage.user_email].forEach(async (email) => {
                     emailjs.send("service_fnwr405", "template_u839qq8", {
                         to_name: email,
                         from_name: "WorkEase",
-                        message: "You have a new booking request for " + this.job.job_name + " on " + this.dateandtime + ". Please check your dashboard for more details.",                
+                        message: "You have a new booking request for " + this.job.job_name + " on " + this.dateandtime + ". Please check your dashboard for more details.",
                     })
-                    .then((response) => {
-                        console.log("SUCCESS!", response.status, response.text);
-                    }).catch((error) => {
-                        console.log("FAILED...", error);
-                    });
+                        .then((response) => {
+                            console.log("SUCCESS!", response.status, response.text);
+                        })
+                        .catch((error) => {
+                            console.log("FAILED...", error);
+                        });
                 });
-                
+
             } else {
 
                 emailjs.send("service_fnwr405", "template_u839qq8", {
@@ -94,16 +96,17 @@ export default {
                     from_name: "WorkEase",
                     message: "You have a new booking request for " + this.job.job_name + " on " + this.dateandtime + ". Please check your dashboard for more details.",
                 })
-                .then((response) => {
-                    console.log("SUCCESS!", response.status, response.text);
-                }).catch((error) => {
-                    console.log("FAILED...", error);
-                });
-                
+                    .then((response) => {
+                        console.log("SUCCESS!", response.status, response.text);
+                    }).catch((error) => {
+                        console.log("FAILED...", error);
+                    });
+
             }
         },
 
         async book() {
+            const router = useRouter()
 
             console.log("Test Name", localStorage);
 
@@ -125,7 +128,12 @@ export default {
 
             await this.sendEmail();
 
-            window.location.href = '/';
+            // window.location.href = '/';
+            // router.push('/')
+            setTimeout(() => {
+                this.router.push("/");
+            }, 3000); // 2000 milliseconds = 2 seconds
+            // this.router.push("/")
         },
 
         async handleCheckout() {
@@ -161,135 +169,136 @@ export default {
     <Navbar />
 
     <!-- <div v-for="job in jobs"> -->
-        <div class="breadcrumb-section section bg_color--5 pt-100 pt-sm-50 pt-xs-40 pb-60 pb-sm-50 pb-xs-40">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="page-breadcrumb-content">
+    <div class="breadcrumb-section section bg_color--5 pt-100 pt-sm-50 pt-xs-40 pb-60 pb-sm-50 pb-xs-40">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="page-breadcrumb-content">
 
-                            <h1>{{ job.job_name }}</h1>
-                        </div>
+                        <h1>{{ job.job_name }}</h1>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Breadcrumb Section Start -->
+    </div>
+    <!-- Breadcrumb Section Start -->
 
-        <!-- Job Meta Detail Box Section Start -->
-        <div class="job-meta-detail-box section bg_color--5 pb-120 pb-lg-100 pb-md-80 pb-sm-60 pb-xs-50">
-            <div class="container">
-                <div class="row g-0">
+    <!-- Job Meta Detail Box Section Start -->
+    <div class="job-meta-detail-box section bg_color--5 pb-120 pb-lg-100 pb-md-80 pb-sm-60 pb-xs-50">
+        <div class="container">
+            <div class="row g-0">
 
-                    <div class="col-lg-4 col-md-6">
-                        <!-- Single Meta Field Start -->
-                        <div class="single-meta-field">
-                            <div class="field-label">
-                                <i class="lnr lnr-map-marker"></i>
-                                <span>Company Name</span>
-                            </div>
-                            <div class="field-value">{{ job.name }}</div>
+                <div class="col-lg-4 col-md-6">
+                    <!-- Single Meta Field Start -->
+                    <div class="single-meta-field">
+                        <div class="field-label">
+                            <i class="lnr lnr-map-marker"></i>
+                            <span>Company Name</span>
                         </div>
-                        <!-- Single Meta Field Start -->
+                        <div class="field-value">{{ job.name }}</div>
                     </div>
-
-                    <div class="col-lg-4 col-md-6">
-                        <!-- Single Meta Field Start -->
-                        <div class="single-meta-field">
-                            <div class="field-label">
-                                <i class="lnr lnr-layers"></i>
-                                <span>Skills </span>
-                            </div>
-                            <div class="field-value">
-                                <div class="job-skill-tag">
-                                    <a href="#">{{ job.skills }}</a>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Single Meta Field Start -->
-                    </div>
-
-
-
-                    <div class="col-lg-4 col-md-6">
-                        <!-- Single Meta Field Start -->
-                        <div class="single-meta-field">
-                            <div class="field-label">
-                                <i class="lnr lnr-tag"></i>
-                                <span>Job Type </span>
-                            </div>
-                            <div class="field-value"><a href="#">{{ job.job_type }}</a></div>
-                        </div>
-                        <!-- Single Meta Field Start -->
-                    </div>
-
-
-
-
-
+                    <!-- Single Meta Field Start -->
                 </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <!-- Single Meta Field Start -->
+                    <div class="single-meta-field">
+                        <div class="field-label">
+                            <i class="lnr lnr-layers"></i>
+                            <span>Skills </span>
+                        </div>
+                        <div class="field-value">
+                            <div class="job-skill-tag">
+                                <a href="#">{{ job.skills }}</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Single Meta Field Start -->
+                </div>
+
+
+
+                <div class="col-lg-4 col-md-6">
+                    <!-- Single Meta Field Start -->
+                    <div class="single-meta-field">
+                        <div class="field-label">
+                            <i class="lnr lnr-tag"></i>
+                            <span>Job Type </span>
+                        </div>
+                        <div class="field-value"><a href="#">{{ job.job_type }}</a></div>
+                    </div>
+                    <!-- Single Meta Field Start -->
+                </div>
+
+
+
+
+
             </div>
         </div>
-        <!-- Job Meta Detail Box Section End -->
+    </div>
+    <!-- Job Meta Detail Box Section End -->
 
-        <!-- Job Details Section Start -->
-        <div
-            class="job-details-section section pt-120 pt-lg-100 pt-md-80 pt-sm-50 pt-xs-40 pb-120 pb-lg-100 pb-md-80 pb-sm-60 pb-xs-50">
-            <div class="container">
-                <div class="row">
+    <!-- Job Details Section Start -->
+    <div
+        class="job-details-section section pt-120 pt-lg-100 pt-md-80 pt-sm-50 pt-xs-40 pb-120 pb-lg-100 pb-md-80 pb-sm-60 pb-xs-50">
+        <div class="container">
+            <div class="row">
 
-                    <div class="col-lg-4 order-lg-2 order-2 mt-sm-60 mt-xs-50">
-                        <div class="sidebar-wrapper-three">
-                            <div class="common-sidebar-widget sidebar-three">
-                                <div class="sidebar-job-apply">
-                                    <div class="action-button">
+                <div class="col-lg-4 order-lg-2 order-2 mt-sm-60 mt-xs-50">
+                    <div class="sidebar-wrapper-three">
+                        <div class="common-sidebar-widget sidebar-three">
+                            <div class="sidebar-job-apply">
+                                <div class="action-button">
 
-                                        <StripeElements v-if="stripeLoaded" v-slot="{ elements, instance }" ref="elms"
-                                            :stripe-key="stripeKey" :instance-options="instanceOptions"
-                                            :elements-options="elementsOptions">
-                                            <StripeElement ref="card" :elements="elements" :options="cardOptions" />
-                                        </StripeElements>
-                                        
-                                        <!-- <button type="button" @click="pay">Pay</button> -->
+                                    <StripeElements v-if="stripeLoaded" v-slot="{ elements, instance }" ref="elms"
+                                        :stripe-key="stripeKey" :instance-options="instanceOptions"
+                                        :elements-options="elementsOptions">
+                                        <StripeElement ref="card" :elements="elements" :options="cardOptions" />
+                                    </StripeElements>
 
-                                        <label for="dateandtime">Date & Time</label>
-                                        <br>
-                                        <input type="datetime-local" v-model="dateandtime" required>
-                                        <br>
-                                        <br>
-                                        <button class="ht-btn text-center" :disabled="!dateandtime" :class="{ 'ht-btn--disabled': !dateandtime }" @click="book">Book Now
-                                            <i class="ml-10 mr-0 fa fa-paper-plane"></i>
-                                        </button>
-                                    </div>
+                                    <!-- <button type="button" @click="pay">Pay</button> -->
+
+                                    <label for="dateandtime">Date & Time</label>
+                                    <br>
+                                    <input type="datetime-local" v-model="dateandtime" required>
+                                    <br>
+                                    <br>
+                                    <button class="ht-btn text-center" :disabled="!dateandtime"
+                                        :class="{ 'ht-btn--disabled': !dateandtime }" @click="book">Book Now
+                                        <i class="ml-10 mr-0 fa fa-paper-plane"></i>
+                                    </button>
                                 </div>
                             </div>
+                        </div>
 
-                            <div class="common-sidebar-widget sidebar-three">
-                                <div class="sidbar-image">
-                                    <a href="#">
-                                        <!-- <img src="assets/images/banner/ads-two.jpg" alt=""> -->
-                                    </a>
-                                </div>
+                        <div class="common-sidebar-widget sidebar-three">
+                            <div class="sidbar-image">
+                                <a href="#">
+                                    <!-- <img src="assets/images/banner/ads-two.jpg" alt=""> -->
+                                </a>
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <div class="col-lg-8 order-lg-1 order-1 pr-55 pr-md-15 pr-sm-15 pr-xs-15">
+                <div class="col-lg-8 order-lg-1 order-1 pr-55 pr-md-15 pr-sm-15 pr-xs-15">
 
-                        <div class="job-detail-content">
-                            <div class="field-descriptions mb-60 mb-sm-30 mb-xs-30">
-                                <h3>Job Description</h3>
+                    <div class="job-detail-content">
+                        <div class="field-descriptions mb-60 mb-sm-30 mb-xs-30">
+                            <h3>Job Description</h3>
 
-                                <p>{{ job.jobdescription }}</p>
+                            <p>{{ job.jobdescription }}</p>
 
-                            </div>
                         </div>
-
                     </div>
 
                 </div>
+
             </div>
         </div>
-        <!-- Job Details Section End -->
+    </div>
+    <!-- Job Details Section End -->
     <!-- </div> -->
 
     <Footer />
